1ed1199c25599667c8c6a1bffd56e9ae70514ca8
Christian Sheen <csheen1725@gmail.com>
2022-01-28 08:51:15 -0600

    Creating userSlice via redux => setting state.value to action.payload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import "./App.css";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+const store = configureStore({
+  reducer: {},
+});
 
 function App() {
   return (
-    <div className="App">
-      <h1>Redux State Tracking Application</h1>
-      <div className="container">
-        <Profile />
-        <Login />
+    <Provider store={store}>
+      <div className="App">
+        <h1>Redux State Tracking Application</h1>
+        <div className="container">
+          <Profile />
+          <Login />
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
