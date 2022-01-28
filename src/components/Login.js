d97ff0be09b5e4e879d585e1e607064947eb590b
Christian Sheen <csheen1725@gmail.com>
2022-01-28 11:37:13 -0600

    Must complete other redux examples for more clarity

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,37 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../redux/user";
+import { logout } from "../redux/user";
 
 function Login() {
+  const dispatch = useDispatch();
   return (
     <div>
-      <button className="login-button">Login</button>
+      <div className="login-out-button-container">
+        <button
+          className="login-button"
+          onClick={() => {
+            dispatch(
+              login({
+                name: "Christian",
+                age: 31,
+                email: "csheen@watchsytems.com",
+              })
+            );
+          }}
+        >
+          Login
+        </button>
+
+        <button
+          className="logout-button"
+          onClick={() => {
+            dispatch(logout());
+          }}
+        >
+          Logout
+        </button>
+      </div>
     </div>
   );
 }
